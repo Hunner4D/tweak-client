0aed701f0a0762a8b7dab94186b1925e57b857ea
Hunner4D <h.herrera.academics@gmail.com>
2020-07-24 20:38:49 -0700

    pre change to hls.js

--- src/components/streams/VideoPlayer.js
+++ src/components/streams/VideoPlayer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import videojs from "video.js";
-// import { HlsSourceHandler } from 'videojs-contrib-hls';
+import { HlsSourceHandler } from 'videojs-contrib-hls';
 import server from "../../apis/server";
 
 class VideoPlayer extends React.Component {
@@ -16,7 +16,7 @@ class VideoPlayer extends React.Component {
     server.get(`/rtmp/${this.props.uuid}`).then((res) => {
       console.log(res.data);
       this.setState({ stream: true });
-      // videojs.getTech('html5').registerSourceHandler(HlsSourceHandler('html5'), 0);
+      videojs.getTech('html5').registerSourceHandler(HlsSourceHandler('html5'), 0);
       
       this.player = videojs(this.videoNode, res.data, function onPlayerReady() {
         console.log("onPlayerReady", this);
